98feaafb6f7770977c345e94d2e95adda84761a5
torresgomezedgar <edgartorresgomez@gmail.com>
2025-10-02 00:26:01 -0500

    deploy de dist

---
+++ dist/assets/2025-10-01-mi-primer-post-B6fwFPDE.js
@@ -0,0 +1,9 @@
+const e=`---
+title: Mi primer post
+date: 2025-10-01
+image: /images/uploads/whatsapp-image-2025-02-11-at-9.58.11-am.jpeg
+---
+Este es mi primer post desde **Decap CMS** 🚀
+
+![imagen](/images/uploads/20250925_104026.jpg)
+`;export{e as default};
